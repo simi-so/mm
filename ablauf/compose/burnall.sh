60d9df8be8cc25e52f0d921c6742e38d09660f9a
uncoyote <jeker.git@outlook.de>
2020-02-06 13:57:12 +0100

    Dok alpha workflow

--- ablauf/compose/burnall.sh
+++ ablauf/compose/burnall.sh
@@ -11,7 +11,7 @@ do
   
   name_suffix=$(echo $f | cut -d/ -f3) # split filepath with delimiter /, and pick 3rd element of result array
   name=$(echo $name_suffix | cut -d. -f1) # split filename with delimiter ., and pick first element of result array
-  out=../generated/${name}.svg
+  out=../generated/${name}.png
 
   cat $f | docker run --rm -i puml_burner > $out
 done
